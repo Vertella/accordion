e65546e456d565a41fa059498aeda8a916f70769
Vertella <128084486+Vertella@users.noreply.github.com>
2023-12-07 23:04:15 +0100

    FAQ

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-// A function that adds and remove the class "active" on the section you click on.
-function toggle(e) {
-  const element = e.target;
-  element.classList.toggle("active");
+async function dataData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const jsonData = await response.json();
+
+  jsonData.forEach(entry => {
+    let infoBox = document.getElementsByClassName("infobox")[0];
+    let newBox = infoBox.cloneNode(true);
+    
+    const title = entry.title;
+    const description = entry.body;
+
+    const accordion = newBox.getElementsByClassName('accordion')[0];
+    accordion.textContent = title;
+
+    const panel = newBox.getElementsByClassName('panel')[0];
+    panel.textContent = description;
+
+    let faq = document.getElementById("faq");
+    faq.appendChild(newBox);
+  });
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-section1Element.addEventListener("click", toggle);
+
+
+
+addEventListener("load", async (event) => {
+  await dataData();
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } 
+      else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
